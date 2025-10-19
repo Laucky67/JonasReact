e2e8a9dceb6ee75456ba167ac6384e8aceb56d60
Laucky <laucky67@outlook.com>
2025-10-20 02:09:37 +0800

    FAST pizaa 组件搭建

--- 03-01-pizza-menu/src/index.js
+++ 03-01-pizza-menu/src/index.js
@@ -13,19 +13,6 @@ function App() {
   );
 }
 
-function Pizza(props) {
-  return (
-    <div className="pizza">
-      <img src={props.photoName} alt="" />
-      <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredints}</p>
-        <span>{props.price + 3}</span>
-      </div>
-    </div>
-  );
-}
-
 function Header() {
   return (
     <header className="header">
@@ -36,27 +23,62 @@ function Header() {
 
 function Menu() {
   return (
-    <main className="menu">
+    <div className="menu">
       <h2>My Menu</h2>
-      {pizzaData.map((pizza) => (
-        <Pizza
-          key={pizza.name}
-          name={pizza.name}
-          ingredints={pizza.ingredients}
-          price={pizza.price}
-          photoName={pizza.photoName}
-        />
-      ))}
-    </main>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Pizza({ pizzaObj }) {
+  // if (pizzaObj.soldOut) return null;
+
+  return (
+    <li className={`pizza ${pizzaObj.soldOut&&"sold-out"}`}>
+      <img src={pizzaObj.photoName} alt="" />
+      <div >
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredints}</p>
+        <span>{pizzaObj.soldOut?"Sold Out":pizzaObj.price + 3}</span>
+      </div>
+    </li>
   );
 }
 
 function Footer() {
+  const hourNow = new Date().getHours();
+  const openHour = 0;
+  const closeHour = 22;
+
+  const isOpen = hourNow >= openHour && hourNow < closeHour;
+
   return (
     <footer className="footer">
       {new Date().toLocaleDateString()}. Welcome to my Pizza Shop
+      <br />
+      {isOpen ? <Order hourNow={hourNow}/> : (
+        <div className="order">
+          <p>
+            We will be back in {openHour}:00 to {closeHour}:00
+          </p>
+        </div>
+      )}
     </footer>
   );
+}
+
+function Order(props){
+  return(
+        <div className="order">
+          <div>当前是{props.hourNow}点，正常营业中</div>
+          <button className="btn">点单</button>
+        </div>
+      )
+
 }
 
 // React v18
