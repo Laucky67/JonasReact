59466d2412403446c43c1131285da4c61483e8c5
Laucky <laucky67@outlook.com>
2025-10-22 15:20:23 +0800

    完成travel-list项目的表单添加、复选框、底栏计算，清单排序和全部删除

--- 05-01-travel-list/src/App.js
+++ 05-01-travel-list/src/App.js
@@ -7,12 +7,41 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleDeleteALL() {
+    const confirm = window.confirm("确定需要清空清单吗？")
+    if(confirm)
+    setItems([]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Footer />
+      <Form onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onDeleteAll={handleDeleteALL}
+      />
+      <Footer items={items} />
     </div>
   );
 }
@@ -21,18 +50,30 @@ function Logo() {
   return <h1>🌴 FAR AWAY 👜</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return null;
+
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+
+    onAddItem(newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3> 你需要为你的旅游😍准备什么？</h3>
-      <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -50,33 +91,80 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem, onToggleItem, onDeleteAll }) {
+  // 定义排序状态
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  // 计算排序数组
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {/* 用排序后的数组渲染子组件 */}
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
+
+      <div
+        className="actions"
+        value={sortBy}
+        onChange={(e) => setSortBy(e.target.value)}
+      >
+        <select>
+          <option value="input">按输入顺序排序</option>
+          <option value="description">按描述排序</option>
+          <option value="packed">按打包状态排序</option>
+        </select>
+        <button onClick={onDeleteAll}>删除所有</button>
+      </div>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
+        {item.quantity} 份 {item.description}
       </span>
-      <button>{item.packed ? "✅" : "❌"}</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Footer() {
+function Footer({ items }) {
+  const numItem = items.length;
+  const packedNumItemc = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedNumItemc / numItem) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        You have {numItem} items on your list, and you already packed{" "}
+        {packedNumItemc} ({percentage}%)
+      </em>
     </footer>
   );
 }
